a3f5c4f64ba695425ba1dcf2140088abce59c963
LokeshReddy Pothapu <lokesh@LokeshReddys-MacBook-Pro.local>
2018-02-04 00:15:53 -0800

    Completed my own custom LokiFramework

---
+++ LokiFramework/LokiFramework/LokiFramework.h
@@ -0,0 +1,19 @@
+//
+//  LokiFramework.h
+//  LokiFramework
+//
+//  Created by LokeshReddy Pothapu on 2/4/18.
+//  Copyright © 2018 LokeshReddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LokiFramework.
+FOUNDATION_EXPORT double LokiFrameworkVersionNumber;
+
+//! Project version string for LokiFramework.
+FOUNDATION_EXPORT const unsigned char LokiFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LokiFramework/PublicHeader.h>
+
+
---
+++ LokiFramework/LokiFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  LokiFramework
+//
+//  Created by LokeshReddy Pothapu on 2/4/18.
+//  Copyright © 2018 LokeshReddy. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() { }
+    
+    public static func printMyMessage() -> String {
+        return "Hey dude Loki framework is designed using custom framework and published upto cocoapods :-)"
+    }
+}
